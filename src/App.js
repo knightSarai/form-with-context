e9e2ebb2ab68bf81ec5391b42f5b5e88b54c3899
knightSarai <knight.sarai.dev@gmail.com>
2020-08-27 16:50:53 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import {ThemeProvider} from './contexts/ThemeContext';
 import {LanguageProvider} from './contexts/LanguageContext';
+
 import PageContent from './components/PageContent';
 import Nav from './components/Nav';
 import Form from './components/Form';
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,21 +1,12 @@
-import React, { Component, createContext } from 'react'
-
+import React, {createContext } from 'react';
+import useToggleContext from '../hooks/useToggleState';
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-    constructor(props){
-        super(props);
-        this.state = {isDarkMode: false};
-        this.toggleTheme = this.toggleTheme.bind(this);
-    }
-    toggleTheme(){
-        this.setState({isDarkMode: !this.state.isDarkMode})
-    }
-    render() {
-        return (
-            <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
-                {this.props.children}
-            </ThemeContext.Provider>
-        )
-    }
-}
+export const ThemeProvider = (props) => {
+    const [isDarkMode, toggleTheme] = useToggleContext(false);
+    return (
+        <ThemeContext.Provider value={{isDarkMode, toggleTheme}}>
+            {props.children}
+        </ThemeContext.Provider>
+    )
+};
